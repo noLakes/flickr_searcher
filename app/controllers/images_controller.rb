32861d6e5e11da3a08a0d964963b9dae98551381
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-09-12 01:33:26 -0400

    finished search function

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,10 @@
 class ImagesController < ApplicationController
   
+  def home
+    if params[:image]
+      flickr = Flickr.new ENV["FLICKR_API_KEY"], ENV["FLICKR_SHARED_SECRET"]
+      @photos = flickr.photos.search(:user_id => params[:image][:user_id])
+    end
+  end
+
 end
